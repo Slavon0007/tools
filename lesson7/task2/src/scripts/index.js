525aa9df42c7102963d8be3ac53cb4a1062b3bc6
Slava <slavkaklimov@gmail.com>
2020-02-25 12:48:34 +0200

    plugin todo

---
+++ lesson7/task2/src/scripts/index.js
@@ -0,0 +1,4 @@
+import { setItem, getItem, onStorageChange } from './storage.js';
+import { renderTasksList } from './render.js';
+import { onTasksListChange } from './todoListchenge.js';
+import { addNewTask } from './createTask.js';
