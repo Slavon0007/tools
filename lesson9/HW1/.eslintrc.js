133dd2d35a99280bba15cf9d6da5eacaaf4929a0
Slava <slavkaklimov@gmail.com>
2020-02-28 09:31:34 +0200

    eslint

--- lesson9/HW1/.eslintrc.js
+++ lesson9/HW1/.eslintrc.js
@@ -1,11 +1,11 @@
-module.export = {
+module.exports = {
     extends: 'eslint-config-airbnb-base',
     rules: {
         'no-console': 2,
         'import/prefer-default-export': 0,
+
     },
     env: {
-        browser: true,
-    },
-
-}
+        browser: true
+    }
+};
